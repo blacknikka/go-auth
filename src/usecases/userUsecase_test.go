34baafe9bc19e6bd8e49b96a9d9497eb08d9eae4
Yasuhiro Nishihara <yasuhiro1026a@hotmail.com>
2020-02-04 23:41:18 +0900

    UserUsecaseのGetAllのテストを追加。

---
+++ src/usecases/userUsecase_test.go
@@ -0,0 +1,64 @@
+package usecases
+
+import (
+	"testing"
+
+	"github.com/blacknikka/go-auth/domain/models/users"
+	"github.com/blacknikka/go-auth/domain/repositories"
+)
+
+// for mock
+type fakeUserRepository struct {
+	repositories.UserRepository
+	FakeGetAll     func() (*[]users.User, error)
+	FakeCreateUser func(users.User) (*users.User, error)
+	FakeFindByID   func(int) (*users.User, error)
+	FakeUpdateUser func(users.User) (*users.User, error)
+	FakeDeleteUser func(users.User) error
+}
+
+func (s *fakeUserRepository) GetAll() (*[]users.User, error) {
+	return s.FakeGetAll()
+}
+
+func (s *fakeUserRepository) CreateUser(user users.User) (*users.User, error) {
+	return s.FakeCreateUser(user)
+}
+
+func (s *fakeUserRepository) FindByID(id int) (*users.User, error) {
+	return s.FakeFindByID(id)
+}
+
+func (s *fakeUserRepository) UpdateUser(user users.User) (*users.User, error) {
+	return s.FakeUpdateUser(user)
+}
+
+func (s *fakeUserRepository) DeleteUser(user users.User) error {
+	return s.FakeDeleteUser(user)
+}
+
+func TestGetAll(t *testing.T) {
+	t.Run("GetAll正常系", func(t *testing.T) {
+		spyUserUsecase := &fakeUserRepository{
+			FakeGetAll: func() (*[]users.User, error) {
+				fakeUsers := []users.User{
+					users.User{},
+					users.User{},
+				}
+				return &fakeUsers, nil
+			},
+		}
+
+		usecase := NewUserUseCase(spyUserUsecase)
+
+		resultUsers, err := usecase.GetAll()
+
+		if err != nil {
+			t.Errorf("err should be nil: %v", err)
+		}
+
+		if resultUsers == nil {
+			t.Errorf("Returned USERS shouldn't nil: %v", resultUsers)
+		}
+	})
+}
